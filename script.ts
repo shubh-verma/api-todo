f59b2e75d340fb8207b2b8ccb2fb5a0e90ee1c64
shubh-verma <shubhvcs@gmail.com>
2024-01-29 19:06:57 +0530

    [Add] typeorm

--- script.ts
+++ script.ts
@@ -1,1 +1,18 @@
 console.log("hello world!");
+
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    firstName: string
+
+    @Column()
+    lastName: string
+
+    @Column()
+    age: number
+}
